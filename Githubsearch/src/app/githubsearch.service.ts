b05fef06c0b0d4213e52acbf4a574e9a53645119
Amir Edin <amiredin@yahoo.com>
2019-04-01 12:44:13 +0300

    Add readme

---
+++ Githubsearch/src/app/githubsearch.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient  } from '@angular/common/http';
+import { observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GithubsearchService {
+
+  ;
+  
+ 
+
+  constructor(private http:HttpClient) {
+    
+   }
+   getProfileInfo(username: string){
+    return this.http.get("https://api.github.com/users/" + username  );
+  }
+
+}
